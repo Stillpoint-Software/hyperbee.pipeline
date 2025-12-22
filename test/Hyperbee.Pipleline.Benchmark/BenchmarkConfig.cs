1e927564e24817eb8bda344115bbeec64b631113
annette.findley <annette.varndell@wagglebee.net>
2025-12-22 08:08:01 -0500

    Updated to .net 10, ran tests, benchmarks and centralized package versions

--- test/Hyperbee.Pipleline.Benchmark/BenchmarkConfig.cs
+++ test/Hyperbee.Pipleline.Benchmark/BenchmarkConfig.cs
@@ -1,6 +1,7 @@
 ﻿using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Exporters;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Loggers;
@@ -8,13 +9,21 @@ using BenchmarkDotNet.Order;
 using BenchmarkDotNet.Validators;
 
 namespace Hyperbee.Pipeline.Benchmark;
+
 public class BenchmarkConfig
 {
     public class Config : ManualConfig
     {
         public Config()
         {
-            AddJob( Job.ShortRun );
+            AddJob( Job.ShortRun
+                .WithRuntime( CoreRuntime.Core80 )
+                .WithId( ".NET 8" ) );
+
+            AddJob( Job.ShortRun
+                .WithRuntime( CoreRuntime.Core10_0 )
+                .WithId( ".NET 10" ) );
+
             AddExporter( MarkdownExporter.GitHub );
             AddValidator( JitOptimizationsValidator.DontFailOnError );
             AddLogger( ConsoleLogger.Default );
