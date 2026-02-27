baeb41db9517d322b229572a79b07a3fd91f4cdc
Brenton Farmer <brent.farmer@wagglebee.net>
2026-02-27 13:34:51 -0800

    Refactor: Add AAA comments to PipelineContextThrowIfErrorTests

--- test/Hyperbee.Pipeline.Tests/PipelineContextThrowIfErrorTests.cs
+++ test/Hyperbee.Pipeline.Tests/PipelineContextThrowIfErrorTests.cs
@@ -8,25 +8,30 @@ public class PipelineContextThrowIfErrorTests
     [TestMethod]
     public void ThrowIfError_should_not_throw_when_no_exception()
     {
+        // Arrange
         var context = new PipelineContext();
 
+        // Act & Assert
         context.ThrowIfError(); // should not throw
     }
 
     [TestMethod]
     public void ThrowIfError_should_throw_when_exception_is_set()
     {
+        // Arrange
         var context = new PipelineContext
         {
             Exception = new InvalidOperationException( "test error" )
         };
 
+        // Act & Assert
         Assert.ThrowsExactly<InvalidOperationException>( () => context.ThrowIfError() );
     }
 
     [TestMethod]
     public void ThrowIfError_should_preserve_original_stack_trace()
     {
+        // Arrange
         var context = new PipelineContext();
 
         try
@@ -38,6 +43,7 @@ public class PipelineContextThrowIfErrorTests
             context.Exception = ex;
         }
 
+        // Act
         try
         {
             context.ThrowIfError();
@@ -45,6 +51,7 @@ public class PipelineContextThrowIfErrorTests
         }
         catch ( InvalidOperationException ex )
         {
+            // Assert
             Assert.Contains( nameof( ThrowFromNestedMethod ), ex.StackTrace! );
         }
     }
